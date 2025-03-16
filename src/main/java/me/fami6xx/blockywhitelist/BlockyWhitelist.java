67c6aaf85f4d1f73763ae4cd9936668838152a76
Fami6Xx <libojer@gmail.com>
2025-03-16 14:44:23 +0100

    feat: refactor BlockyWhitelist to integrate JDA for Discord command handling and improve setup validation

--- src/main/java/me/fami6xx/blockywhitelist/BlockyWhitelist.java
+++ src/main/java/me/fami6xx/blockywhitelist/BlockyWhitelist.java
@@ -1,33 +1,45 @@
 package me.fami6xx.blockywhitelist;
 
+import me.fami6xx.blockywhitelist.discord.DiscordCommandListener;
 import me.fami6xx.rpuniverse.core.misc.utils.FamiUtils;
-import net.essentialsx.api.v2.services.discord.DiscordService;
-import net.essentialsx.api.v2.services.discord.InteractionException;
-import net.essentialsx.api.v2.services.discord.InteractionMember;
-import net.essentialsx.api.v2.services.discordlink.DiscordLinkService;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.exceptions.InvalidTokenException;
+import net.dv8tion.jda.api.interactions.DiscordLocale;
+import net.dv8tion.jda.api.interactions.IntegrationType;
+import net.dv8tion.jda.api.interactions.InteractionContextType;
+import net.dv8tion.jda.api.interactions.commands.Command;
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.localization.LocalizationFunction;
+import net.dv8tion.jda.api.interactions.commands.localization.LocalizationMap;
+import net.dv8tion.jda.api.utils.data.DataObject;
+import net.milkbowl.vault.permission.Permission;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.UnmodifiableView;
 
 import java.io.File;
+import java.util.*;
 
 public final class BlockyWhitelist extends JavaPlugin implements Listener {
-    private DiscordService discordService = Bukkit.getServicesManager().load(DiscordService.class);
-    private DiscordLinkService discordLinkService = Bukkit.getServicesManager().load(DiscordLinkService.class);
     private JSONStore jsonStore;
+    private Permission perms;
+    private JDA jda;
+    private Guild guild;
 
     @Override
     public void onEnable() {
-        if (discordService == null) {
-            discordService = Bukkit.getServicesManager().load(DiscordService.class);
-        }
-        try {
-            discordService.getInteractionController().registerCommand(new WhitelistSlashCommand());
-        } catch (InteractionException e) {
-            throw new RuntimeException(e);
+        if (!setupPermissions()) {
+            getLogger().severe("Failed to hook Vault, disabling plugin");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
         }
 
         if (!getDataFolder().exists()) {
@@ -37,6 +49,26 @@ public final class BlockyWhitelist extends JavaPlugin implements Listener {
         File file = new File(getDataFolder(), "data.json");
 
         jsonStore = JSONStore.load(file);
+
+        if (jsonStore == null) {
+            getLogger().severe("Failed to load data.json");
+            getLogger().severe("Please delete BlockyWhitelist folder and restart the server");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        boolean firstSetup = jsonStore.botToken.isEmpty() || jsonStore.guildId.isEmpty();
+        if (firstSetup) {
+            getLogger().info("First setup detected, please configure the plugin in-game");
+        } else {
+            getLogger().info("Bot token and guild ID loaded successfully");
+            List<String> errors = loadJDA();
+            if (!errors.isEmpty()) {
+                getLogger().severe("Failed load JDA:");
+                errors.forEach(getLogger()::severe);
+                return;
+            }
+        }
 
         getServer().getPluginManager().registerEvents(this, this);
 
@@ -49,85 +81,93 @@ public final class BlockyWhitelist extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        final Player player = event.getPlayer();
-
-        if (player.hasPermission("blockywhitelist.ignore")) {
+    public void onPreLogin(AsyncPlayerPreLoginEvent event) {
+        if (jsonStore.botToken.isEmpty() || jsonStore.guildId.isEmpty() || jda == null || guild == null || jsonStore.addedRoles.isEmpty() || jsonStore.allowedRoles.isEmpty()) {
             return;
         }
 
-        final String discordId = getDiscordId(player);
-        if (discordId == null) {
-            kickPlayer(player);
-            return;
-        }
-
-        InteractionMember member = getDiscordService().getMemberById(discordId).join();
-        if (member == null) {
-            kickPlayer(player);
-            return;
-        }
-
-        boolean whitelisted = false;
-        for (String role : jsonStore.addedRoles) {
-            if (member.hasRole(role)) {
-                whitelisted = true;
-                break;
-            }
-        }
-
-        if (!whitelisted) {
-            kickPlayer(player);
-        }
+        // JDA is ready, check if the player is whitelisted
     }
 
-    private void kickPlayer(Player player) {
-        player.kickPlayer(FamiUtils.format("&b&lBlockyWhitelist\n\n&r&cMusis projit whitelistem.\n\n&r&7Prosim pripoj se na nas discord a udelej si WhiteList."));
+    /**
+     * Trys to load JDA and connect to the discord server.
+     *
+     * @return List of errors that occurred during the connection.
+     */
+    public List<String> loadJDA() {
+        List<String> errors = new ArrayList<>();
+        try {
+            jda = JDABuilder.createDefault(jsonStore.botToken).build().awaitReady();
+            guild = jda.getGuildById(jsonStore.guildId);
+            if (guild == null) {
+                throw new IllegalStateException("Failed to connect to Discord server");
+            }
+            guild.upsertCommand("link", "Linkne váš Discord účet s MC účtem.")
+                    .addOption(OptionType.STRING, "code", "Váš kód, zobrazen při napojení na MC server.", true)
+                    .queue();
+
+            guild.upsertCommand("whitelist", "Whitelistne hráče.")
+                    .addOption(OptionType.USER, "user", "Hráč, kterého chcete whitelistnout.", true, true)
+                    .queue();
+
+            guild.upsertCommand("failed", "Hráči se nepodařil pokus o whitelist.")
+                    .addOption(OptionType.INTEGER, "attempt", "Kolikátý pokus o whitelist to je.", true)
+                    .addOption(OptionType.USER, "user", "Hráč, kterého se pokus týká.", true)
+                    .queue();
+
+            guild.getMemberById()
+
+            jda.addEventListener(new DiscordCommandListener());
+        } catch (InvalidTokenException | IllegalArgumentException | IllegalStateException | InterruptedException e) {
+            if (e instanceof InvalidTokenException) {
+                errors.add("Invalid bot token");
+            } else if (e instanceof IllegalArgumentException) {
+                errors.add("Bot token is empty");
+            } else if (e instanceof IllegalStateException) {
+                errors.add("Failed to connect to Discord server, check Guild Id.");
+            } else {
+                errors.add("Failed initializing JDA: " + e.getMessage());
+            }
+        }
+        return errors;
+    }
+
+    private String getKickMessageNotWhitelisted() {
+        return FamiUtils.format("&b&lBlockyWhitelist\n\n&r&cMusis projit whitelistem.\n\n&r&7Prosim pripoj se na nas discord a udelej si WhiteList.");
+    }
+
+    private String getKickMessageNotSetup() {
+        return FamiUtils.format("&b&lBlockyWhitelist\n\n&r&cServer neni nastaven.\n\n&r&7Prosim kontaktuj admina.");
     }
 
     public JSONStore getJsonStore() {
         return jsonStore;
     }
 
+    public JDA getJDA() {
+        return jda;
+    }
+
+    public Guild getGuild() {
+        return guild;
+    }
+
+    /**
+     * Setup Vault permissions by obtaining the Permission provider.
+     */
+    private boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        if (rsp != null) {
+            perms = rsp.getProvider();
+        }
+        return perms != null;
+    }
+
+    /**
+     * Gets the Instance of the BlockyWhitelist plugin.
+     * @return Instance of the BlockyWhitelist plugin.
+     */
     public static BlockyWhitelist getInstance() {
         return getPlugin(BlockyWhitelist.class);
-    }
-
-    public static DiscordService getDiscordService() {
-        DiscordService service = Bukkit.getServicesManager().load(DiscordService.class);
-        if (service == null) {
-            getInstance().getLogger().severe("Discord service not found!");
-            throw new RuntimeException("Discord service not found!");
-        }
-        return service;
-    }
-
-    public static DiscordLinkService getDiscordLinkService() {
-        DiscordLinkService service = Bukkit.getServicesManager().load(DiscordLinkService.class);
-        if (service == null) {
-            getInstance().getLogger().severe("DiscordLink service not found!");
-            throw new RuntimeException("DiscordLink service not found!");
-        }
-        return service;
-    }
-
-    public static String getDiscordTag(final Player player) {
-        final String discordId = getDiscordLinkService().getDiscordId(player.getUniqueId());
-        if (discordId == null) {
-            return null;
-        }
-
-        final InteractionMember member = getDiscordService().getMemberById(discordId).join();
-        return member == null ? null : member.getTag();
-    }
-
-    public static String getDiscordId(final Player player) {
-        final String discordId = getDiscordLinkService().getDiscordId(player.getUniqueId());
-        if (discordId == null) {
-            return null;
-        }
-
-        final InteractionMember member = getDiscordService().getMemberById(discordId).join();
-        return member == null ? null : member.getId();
     }
 }
