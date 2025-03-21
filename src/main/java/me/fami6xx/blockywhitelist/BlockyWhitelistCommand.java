d28cda98cfb00f4caa8d66f5a0d851d34cabc984
Fami6Xx <libojer@gmail.com>
2025-03-21 10:28:26 +0100

    refactor: update imports in BlockyWhitelistCommand to use new utility structure

--- src/main/java/me/fami6xx/blockywhitelist/BlockyWhitelistCommand.java
+++ src/main/java/me/fami6xx/blockywhitelist/BlockyWhitelistCommand.java
@@ -1,8 +1,8 @@
 package me.fami6xx.blockywhitelist;
 
 import me.fami6xx.blockywhitelist.menus.ChooseNextStepMenu;
-import me.fami6xx.rpuniverse.RPUniverse;
-import me.fami6xx.rpuniverse.core.misc.utils.FamiUtils;
+import me.fami6xx.blockywhitelist.utils.FamiUtils;
+import me.fami6xx.blockywhitelist.utils.languages.Lang;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,17 +12,17 @@ public class BlockyWhitelistCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)){
-            sender.sendMessage(FamiUtils.format("&c&lERROR! &7" + RPUniverse.getLanguageHandler().errorOnlyPlayersCanUseThisCommandMessage));
+            sender.sendMessage(FamiUtils.format("&c&lERROR! &7" + Lang.errorOnlyPlayersCanUseThisCommandMessage));
             return true;
         }
         Player player = (Player) sender;
 
         if (!player.hasPermission("blockywhitelist.use")){
-            player.sendMessage(FamiUtils.format("&c&lERROR! &7" + RPUniverse.getLanguageHandler().errorYouDontHavePermissionToUseThisCommandMessage));
+            player.sendMessage(FamiUtils.format("&c&lERROR! &7" + Lang.errorYouDontHavePermissionToUseThisCommandMessage));
             return true;
         }
 
-        new ChooseNextStepMenu(RPUniverse.getInstance().getMenuManager().getPlayerMenu(player)).open();
+        new ChooseNextStepMenu(BlockyWhitelist.getInstance().getMenuManager().getPlayerMenu(player)).open();
         return true;
     }
 }
