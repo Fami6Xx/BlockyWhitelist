9664f0969530742828ec2b6612b2fddebe7fa90b
Fami6Xx <libojer@gmail.com>
2025-03-21 10:22:22 +0100

    feat: update language strings for pagination and menu interactions in Lang class

--- src/main/java/me/fami6xx/blockywhitelist/utils/languages/Lang.java
+++ src/main/java/me/fami6xx/blockywhitelist/utils/languages/Lang.java
@@ -1,6 +1,13 @@
 package me.fami6xx.blockywhitelist.utils.languages;
 
 public class Lang {
-    public static String prefix = "&8[&bBW&8] &7";
-
+    public static String prefix = "&b&lBW& &8» &7";
+    public static String previousPageItemDisplayName = "&bPrevious page";
+    public static String previousPageItemLore = "&7Click to go to the previous page.";
+    public static String nextPageItemDisplayName = "&bNext page";
+    public static String nextPageItemLore = "&7Click to go to the next page.";
+    public static String closeItemDisplayName = "&cClose";
+    public static String closeItemLore = "&7Click to close this menu.";
+    public static String errorMenuAlreadyOnFirstPage = "&cYou are already on the first page.";
+    public static String errorMenuAlreadyOnLastPage = "&cYou are already on the last page.";
 }
