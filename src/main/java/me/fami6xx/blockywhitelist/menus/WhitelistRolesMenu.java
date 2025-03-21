3baaba2d56d23c6ff5a3a651cea03785c7b62f5b
Fami6Xx <libojer@gmail.com>
2025-03-21 10:35:41 +0100

    refactor: remove unused imports in WhitelistRolesMenu

--- src/main/java/me/fami6xx/blockywhitelist/menus/WhitelistRolesMenu.java
+++ src/main/java/me/fami6xx/blockywhitelist/menus/WhitelistRolesMenu.java
@@ -13,9 +13,6 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class WhitelistRolesMenu extends EasyPaginatedMenu {
     private final BlockyWhitelist blockyWhitelist;
     private final NamespacedKey blockyWhitelistKey;
